9146a0a4a99e4940e7ba20f168b1f4f84c824a3e
Yusuke Nakamura <yusuke1994525@gmail.com>
2014-04-02 21:16:21 +0900

    各ボタンが押された時の関数定義
    StoryBoardのCustom ClassをTitleViewControllerにする。
    StoryBoardから要素の右クリックで
    touch Up insideを選択し、TitleViewController.hまでもってくる

---
+++ PhotoShift/TitleViewController.h
@@ -0,0 +1,19 @@
+//
+//  TitleViewController.h
+//  PhotoShift
+//
+//  Created by yusuke on 4/2/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TitleViewController : UIViewController
+
+
+//各Buttonが押された時呼ばれる関数の定義
+- (IBAction)cameraButtonTaped:(id)sender;
+- (IBAction)camerarollButtonTaped:(id)sender;
+- (IBAction)challangeButtonTaped:(id)sender;
+
+@end
---
+++ PhotoShift/TitleViewController.m
@@ -0,0 +1,57 @@
+//
+//  TitleViewController.m
+//  PhotoShift
+//
+//  Created by yusuke on 4/2/14.
+//
+//
+
+#import "TitleViewController.h"
+
+@interface TitleViewController ()
+
+@end
+
+@implementation TitleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)cameraButtonTaped:(id)sender {
+}
+
+- (IBAction)camerarollButtonTaped:(id)sender {
+}
+
+- (IBAction)challangeButtonTaped:(id)sender {
+}
+@end
